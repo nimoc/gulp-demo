bb01a592226a9cafbfc181343dc03ad1f7cf5a85
nimojs <imhadley@163.com>
2015-04-06 21:09:36 +0800

    .

---
+++ README.md
@@ -0,0 +1,4 @@
+gulp-demo
+=========
+
+请阅读 [使用 gulp 构建一个项目](https://github.com/nimojs/gulp-book/blob/master/chapter7.md)
---
+++ gulpfile.js
@@ -0,0 +1,208 @@
+var gulp = require('gulp')
+var colors = require('colors')
+
+colors.setTheme({
+    silly: 'rainbow',
+    input: 'grey',
+    verbose: 'cyan',
+    prompt: 'grey',
+    info: 'green',
+    data: 'grey',
+    help: 'cyan',
+    warn: 'yellow',
+    debug: 'blue',
+    error: 'red'
+})
+
+var log = function (msg) {
+    // 14:13:55 GMT+0800 (CST)
+    var now = new Date().toTimeString().replace(/\s.*$/, '')
+    var now =  '[' + colors.data(now) + ']'
+    // [10:52:18]
+    console.log(now + ' ' + msg)
+}
+
+var handleError = function (err) {
+    console.log('\n')
+    log(colors.error('Error!'))
+    log('fileName: ' + colors.error(err.fileName))
+    log('lineNumber: ' + colors.error(err.lineNumber))
+    log('message: ' + err.message)
+    log('plugin: ' + colors.info(err.plugin))
+}
+
+var uglify = require('gulp-uglify')
+var watchPath = require('gulp-watch-path')
+var combiner = require('stream-combiner2')
+var sourcemaps = require('gulp-sourcemaps')
+var minifycss = require('gulp-minify-css')
+var autoprefixer = require('gulp-autoprefixer')
+var less = require('gulp-less')
+var sass = require('gulp-ruby-sass')
+var imagemin = require('gulp-imagemin')
+
+gulp.task('watchjs', function () {
+    gulp.watch('src/js/**/*.js', function (event) {
+        var paths = watchPath(event, 'src/', 'dist/')
+        /*
+        paths
+            { srcPath: 'src/js/log.js',
+              srcDir: 'src/js/',
+              distPath: 'dist/js/log.js',
+              distDir: 'dist/js/',
+              srcFilename: 'log.js',
+              distFilename: 'log.js' }
+        */
+        log(colors.info(event.type) + ':' + paths.srcPath)
+        log('dist:' + paths.distPath)
+
+        var combined = combiner.obj([
+            gulp.src(paths.srcPath),
+            sourcemaps.init(),
+            uglify(),
+            sourcemaps.write('./'),
+            gulp.dest(paths.distDir)
+        ])
+
+        combined.on('error', handleError)
+    })
+})
+
+gulp.task('uglifyjs', function () {
+    var combined = combiner.obj([
+        gulp.src('src/js/**/*.js'),
+        sourcemaps.init(),
+        uglify(),
+        sourcemaps.write('./'),
+        gulp.dest('dist/js/')
+    ])
+    combined.on('error', handleError)
+})
+
+
+gulp.task('watchcss', function () {
+    gulp.watch('src/css/**/*.css', function (event) {
+        var paths = watchPath(event, 'src/', 'dist/')
+
+        log(colors.info(event.type) + ':' + paths.srcPath)
+        log('dist:' + paths.distPath)
+
+        gulp.src(paths.srcPath)
+            .pipe(sourcemaps.init())
+            .pipe(autoprefixer({
+              browsers: 'last 2 versions'
+            }))
+            .pipe(minifycss())
+            .pipe(sourcemaps.write('./'))
+            .pipe(gulp.dest(paths.distDir))
+    })
+})
+
+gulp.task('minifycss', function () {
+    gulp.src('src/css/**/*.css')
+        .pipe(sourcemaps.init())
+        .pipe(autoprefixer({
+          browsers: 'last 2 versions'
+        }))
+        .pipe(minifycss())
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('dist/css/'))
+})
+
+gulp.task('watchless', function () {
+    gulp.watch('src/less/**/*.less', function (event) {
+        var paths = watchPath(event, 'src/', 'dist/')
+
+        log(colors.info(event.type) + ':' + paths.srcPath)
+        log('dist:' + paths.distPath)
+        var combined = combiner.obj([
+            gulp.src(paths.srcPath),
+            sourcemaps.init(),
+            autoprefixer({
+              browsers: 'last 2 versions'
+            }),
+            less(),
+            minifycss(),
+            sourcemaps.write('./'),
+            gulp.dest(paths.distDir)
+        ])
+        combined.on('error', handleError)
+    })
+})
+
+gulp.task('lesscss', function () {
+    var combined = combiner.obj([
+            gulp.src('src/less/**/*.less'),
+            sourcemaps.init(),
+            autoprefixer({
+              browsers: 'last 2 versions'
+            }),
+            less(),
+            minifycss(),
+            sourcemaps.write('./'),
+            gulp.dest('dist/css/')
+        ])
+    combined.on('error', handleError)
+})
+
+gulp.task('sasscss', function () {
+        sass('src/sass/')
+        .on('error', function (err) {
+            console.error('Error!', err.message);
+        })
+        .pipe(sourcemaps.init())
+        .pipe(minifycss())
+        .pipe(autoprefixer({
+          browsers: 'last 2 versions'
+        }))
+        .pipe(sourcemaps.write('./'))
+        .pipe(gulp.dest('dist/css'))
+})
+
+gulp.task('watchsass',function () {
+    gulp.watch('src/sass/**/*.scss', ['sasscss']);
+})
+
+
+gulp.task('watchimage', function () {
+    gulp.watch('src/images/**/*', function (event) {
+        var paths = watchPath(event,'src/','dist/')
+
+        log(colors.info(event.type) + ':' + paths.srcPath)
+        log('dist:' + paths.distPath)
+
+        gulp.src(paths.srcPath)
+            .pipe(imagemin({
+                progressive: true
+            }))
+            .pipe(gulp.dest(paths.distDir))
+    })
+})
+
+gulp.task('image', function () {
+    gulp.src('src/images/**/*')
+        .pipe(imagemin({
+            progressive: true
+        }))
+        .pipe(gulp.dest('dist/images'))
+})
+
+gulp.task('watchcopy', function () {
+    gulp.watch('src/fonts/**/*', function (event) {
+        var paths = watchPath(event,'src/', 'dist/')
+
+        log(colors.info(event.type) + ':' + paths.srcPath)
+        log('copy:' + paths.distPath)
+
+        gulp.src(paths.srcPath)
+            .pipe(gulp.dest(paths.distDir))
+    })
+})
+
+gulp.task('copy', function () {
+    gulp.src('src/fonts/**/*')
+        .pipe(gulp.dest('dist/fonts/'))
+})
+
+
+gulp.task('default', ['watchjs', 'watchcss', 'watchless', 'watchsass', 'watchimage', 'watchcopy'])
